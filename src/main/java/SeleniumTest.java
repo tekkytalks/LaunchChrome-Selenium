edfea98b505d572b7e9d6176ad6c970c27622f6d
tekkytalks <tekkytalkschannel@gmail.com>
2022-06-08 15:16:42 +0530

    Initial commit

---
+++ src/main/java/SeleniumTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public void launchbrowser() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.google.com/");
+
+    }
+
+    public static void main(String[] args) {
+
+        SeleniumTest obj =new SeleniumTest();
+        obj.launchbrowser();
+
+    }
+
+}
